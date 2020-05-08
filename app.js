b17c2ceda1a205cba4db07ed6125e740832b7b58
Aragunnar <zellishell@hotmail.com>
2020-05-08 14:32:35 +0300

    Added some routing to App.js
    Added db_connection.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+// Create an Express application
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// Default route
+app.get('/', (req, res) => res.send('Welcome to Pi-NAS'))
+// Write route
+app.route('/write', (req, res) => res.send('Here you can write to the storage'))
+// Read route
+app.route('/read', (req, res) => res.send('Here yu can read the storage'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
-// Serve images, CSS files and JS files in a driectory called public
-app.use(express.static('public'))
+
+  
